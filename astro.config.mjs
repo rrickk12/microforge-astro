ab2f1bdd0806c62c2dbe5bdea6b2dc213a8d30f1
Matt_BF <mbfiamenghi@gmail.com>
2022-12-24 17:32:59 -0300

    managed to link blog to storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
+// storyblok
+import storyblok from "@storyblok/astro";
+// vite
+import { loadEnv } from "vite";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -9,7 +13,25 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image(), svelte(), tailwind()]
+  integrations: [
+    image(),
+    svelte(),
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: "storyblok/BlogPost",
+        blogPostList: "storyblok/BlogPostList",
+        page: "storyblok/Page",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "us",
+      },
+    }),
+  ],
 });
