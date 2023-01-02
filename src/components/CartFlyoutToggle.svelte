f42ce62889c30f96bd40dd61b840de98dc880c83
Matt_BF <mbfiamenghi@gmail.com>
2023-01-02 20:32:04 -0300

    add num products to cart

--- src/components/CartFlyoutToggle.svelte
+++ src/components/CartFlyoutToggle.svelte
@@ -4,9 +4,10 @@
 
 <!--use "$" to read the store value-->
 <button class="flex" on:click={() => isCartOpen.set(!$isCartOpen)}
-  >Cart
+  ><i class="fa-solid fa-cart-shopping text-2xl" />
   {#if Object.values($cartItems).length}
-    <span class="text-xs border rounded-lg"
+    <span
+      class=" bg-teal-900 text-sm font-bold text-white rounded-full h-5 w-5 relative inline-block top-[-12px]"
       >{Object.values($cartItems).length}</span
     >
   {/if}
