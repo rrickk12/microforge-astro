5c12928b7f728af37e66777d60e81748fc598817
Matt <mbfiamenghi@gmail.com>
2023-01-03 11:31:52 -0300

    fixed footer and contact pages

--- src/components/ContactForm.svelte
+++ src/components/ContactForm.svelte
@@ -8,7 +8,7 @@
 <form action="POST" data-netlify="true" class="flex flex-col gap-2 p-8">
   <label class="text-teal-900" for="name">Seu nome*</label>
   <input
-    class="border mb-5"
+    class="border mb-5 p-1"
     name="name"
     type="text"
     placeholder="Nome completo"
@@ -17,7 +17,7 @@
   />
   <label class="text-teal-900" for="email">Seu e-mail*</label>
   <input
-    class="border mb-5"
+    class="border mb-5 p-1"
     type="email"
     placeholder="E-mail"
     bind:value={email}
@@ -27,14 +27,14 @@
     >Telefone de contato/Whatsapp</label
   >
   <input
-    class="border mb-5"
+    class="border mb-5 p-1"
     name="whatsapp"
     type="tel"
     placeholder="WhatsApp"
     bind:value={whatsapp}
   />
   <label class="text-teal-900" for="message">Sua Mensagem*</label>
-  <textarea class="border" placeholder="Mensagem" bind:value={message} />
+  <textarea class="border p-1" placeholder="Mensagem" bind:value={message} />
   <button
     class="bg-teal-900 text-white rounded-xl w-1/2 mt-5 self-center"
     type="submit">Enviar</button
