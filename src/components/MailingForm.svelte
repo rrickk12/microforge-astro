ed1a9420117c961fb20f7b48d70a1f3cf9ed45c7
Matt_BF <mbfiamenghi@gmail.com>
2023-01-02 23:26:23 -0300

    add my own form

---
+++ src/components/MailingForm.svelte
@@ -0,0 +1,35 @@
+<script>
+  let name = "";
+  let email = "";
+</script>
+
+<form
+  class="bg-slate-50 p-2 flex flex-col w-3/4"
+  action="POST"
+  data-netlify="true"
+  id="mailing"
+>
+  <h2 class="text-lg text-teal-900 ">Newsletter</h2>
+  <p class="text-black mb-5">
+    Se inscreva na nossa Newsletter e fique sabendo de notícias em biomol em
+    primeira mão.
+  </p>
+  <input
+    class="mb-5"
+    type="text"
+    placeholder="Nome completo"
+    bind:value={name}
+    required
+  />
+  <input
+    class="mb-5"
+    type="email"
+    placeholder="E-mail"
+    bind:value={email}
+    required
+  />
+  <button
+    class="rounded-xl bg-teal-900 self-center hover:bg-amber-500 w-1/2"
+    type="submit">INSCREVER</button
+  >
+</form>
