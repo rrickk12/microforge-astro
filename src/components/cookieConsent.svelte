2829f6a2032a2a0ae6a51a2127a09bc0801562e1
Matt_BF <mbfiamenghi@gmail.com>
2023-01-08 23:56:41 -0300

    add cookie consent banner

--- src/components/cookieConsent.svelte
+++ src/components/cookieConsent.svelte
@@ -9,17 +9,22 @@
 </script>
 
 {#if !cookieConsent}
-  <div id="consent" class="fixed bottom-0 p-2 w-full bg-white z-50">
-    <p>
+  <div
+    id="consent"
+    class="fixed bottom-0 p-5 w-full flex items-center bg-white z-50"
+  >
+    <p class="mr-5 text-justify">
       Utilizamos cookies para melhorar a experiência no nosso website. Ao
       utilizá-lo você concorda com o uso dos cookies nos termos descritos na
       nossa <span
-        ><a class="mr-5" href="politica">Política de Privacidade</a></span
-      >
-      <button
-        class="p-2 bg-teal-900 hover:bg-amber-500 text-white"
-        on:click={setCookieConsent}>Aceitar</button
+        ><a class="text-teal-900 hover:text-teal-800" href="politica"
+          >Política de Privacidade</a
+        ></span
       >
     </p>
+    <button
+      class="p-2 bg-teal-900 self-center hover:bg-amber-500 text-white"
+      on:click={setCookieConsent}>Aceitar</button
+    >
   </div>
 {/if}
